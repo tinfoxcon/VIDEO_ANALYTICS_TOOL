b5f7b1e829fdded507e7d097fa1e4f6f3072de49
Tinfox Consulting <contact@tinfoxconsulting.com>
2026-06-30 16:35:53 +0530

    CORS Update

--- backend/app/main.py
+++ backend/app/main.py
@@ -23,7 +23,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_origin, "http://localhost:3000", "http://localhost:4173"],
+    allow_origins=[settings.frontend_origin, "http://localhost:3000", "http://localhost:4173", "https://video-analytics-tool-9fyw.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
