36946dd25597cf59060ba0b260cf8b84ad6cbe7a
Tinfox Consulting <contact@tinfoxconsulting.com>
2026-06-30 12:41:06 +0530

    fix comma issue

--- backend/app/main.py
+++ backend/app/main.py
@@ -22,7 +22,7 @@ app = FastAPI(
 )
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[settings.frontend_origin, "http://localhost:3000", "http://localhost:4173" "https://video-analytics-tool-9fyw.onrender.com"],
+    allow_origins=[settings.frontend_origin, "http://localhost:3000", "http://localhost:4173", "https://video-analytics-tool-9fyw.onrender.com"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
